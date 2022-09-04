f1c3bd5edc89ca7a49c4f48a4432ce6ffb4303be
Veljko Gligorijevic <gligorijevicveljko3@gmail.com>
2022-09-04 21:12:36 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,153 @@
+export const placeInfo = [
+    {
+      id: 1,
+      headerTitle: 'Imola Circuit',
+      headerSubTitle: 'Autodromo Internazionale Enzo e Dino Ferrari',
+      headerText:
+        `The Autodromo Internazionale Enzo e Dino Ferrari, better known as Imola, is a motor racing circuit in the town of Imola, in the Emilia-Romagna region of Italy, 40 kilometres (25 mi) east of Bologna. It is one of the few major international circuits to run in an anti-clockwise direction. The circuit is named after Ferrari's late founder, Enzo Ferrari, and his son, Alfredo "Dino" Ferrari, who died in 1956 at age 24. Before Enzo's death, it was called Autodromo Dino Ferrari. The circuit has an FIA Grade One licence.`,
+      title: 'Imola Circuit',
+      description:
+        `The Autodromo Internazionale Enzo e Dino Ferrari, better known as Imola, is a motor racing circuit in the town of Imola, in the Emilia-Romagna region of Italy, 40 kilometres (25 mi) east of Bologna. It is one of the few major international circuits to run in an anti-clockwise direction. The circuit is named after Ferrari's late founder, Enzo Ferrari, and his son, Alfredo "Dino" Ferrari, who died in 1956 at age 24. Before Enzo's death, it was called Autodromo Dino Ferrari. The circuit has an FIA Grade One licence.[2]
+
+        Imola was the venue for the San Marino Grand Prix between 1981 and 2006. During this period, two Grands Prix were held in Italy every year, with the Italian Grand Prix taking place at Monza, so the Imola race was named after the nearby state. Imola also hosted the 1980 Italian Grand Prix in place of Monza. When Formula One visits Imola, it is seen as the home circuit of Scuderia Ferrari, and masses of supporters come out to support the local team.
+        
+        The venue returned to the Formula One calendar during the 2020 season to help the sport fill calendar gaps caused by cancellations of other races due to the COVID-19 pandemic, with the race at the circuit being named the Emilia Romagna Grand Prix in honour of the region where the circuit is located. This also meant the venue hosted a World Championship race under a third different name having hosted the 1980 Italian Grand Prix and the San Marino Grand Prix from 1981 to 2006`,
+      img: '.././images/imola.jpg',
+      maps:
+        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2853.339630991969!2d11.712162215520328!3d44.34407377910367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132b4a7474d71167%3A0x81177e3551c217a6!2z0JDRg9GC0L7QvNC-0LHQuNC70YHQutCwINGB0YLQsNC30LAg0JXQvdGG0L4g0Lgg0JTQuNC90L4g0KTQtdGA0LDRgNC4!5e0!3m2!1ssr!2srs!4v1631487089071!5m2!1ssr!2srs'
+    },
+    {
+      id: 2,
+      headerTitle: 'Circuit de Monaco',
+      headerSubTitle: 'Circuit de Monaco',
+      headerText:
+        `Circuit de Monaco is a street circuit laid out on the city streets of Monte Carlo and La Condamine around the harbour of the Principality of Monaco. It is commonly referred to as "Monte Carlo" because it is largely inside the Monte Carlo neighbourhood of Monaco.`,
+      title: 'Circuit de Monaco',
+      description:
+        `Circuit de Monaco is a street circuit laid out on the city streets of Monte Carlo and La Condamine around the harbour of the Principality of Monaco. It is commonly referred to as "Monte Carlo" because it is largely inside the Monte Carlo neighbourhood of Monaco.
+
+        The circuit is annually used on two weekends in May for Formula One Monaco Grand Prix and Formula E Monaco ePrix (odd years) or Historic Grand Prix of Monaco (even years). Formula One's respective feeder series over the years – Formula 3000, GP2 Series and today the Formula 2 championship – also visit the circuit concurrently with Formula One. The Monaco Grand Prix is one of the three events where victory counts towards the Triple Crown of Motorsport.`,
+      img: '.././images/monaco.jpg',
+      maps:
+        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2882.7447090736737!2d7.4190763157595985!3d43.73662997911852!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12cdc28bfa733ce7%3A0x1002d8bbd550b626!2sMonaco%20Grand%20Prix!5e0!3m2!1ssr!2srs!4v1631487149715!5m2!1ssr!2srs'
+    },
+    {
+      id: 3,
+      headerTitle: 'Baku City Circuit',
+      headerSubTitle: 'Bakı Şəhər Halqası',
+      headerText:
+        `The Baku City Circuit is a motor racing street circuit located in Baku, Azerbaijan constructed near Baku Boulevard. A lap of the circuit is 6.003 kilometres (3.730 mi), making it the third-longest circuit on the Formula One calendar (after Circuit de Spa-Francorchamps and Jeddah Street Circuit). The inaugural Formula One race at the circuit was the 2016 European Grand Prix and its support events. A year later, in 2017, the circuit held the inaugural Azerbaijan Grand Prix. The event is organised by Baku City Circuit Operation Company.`,
+      title:
+        'Baku City Circuit',
+      description:
+      `The Baku City Circuit is a motor racing street circuit located in Baku, Azerbaijan constructed near Baku Boulevard. A lap of the circuit is 6.003 kilometres (3.730 mi), making it the third-longest circuit on the Formula One calendar (after Circuit de Spa-Francorchamps and Jeddah Street Circuit). The inaugural Formula One race at the circuit was the 2016 European Grand Prix and its support events. A year later, in 2017, the circuit held the inaugural Azerbaijan Grand Prix. The event is organised by Baku City Circuit Operation Company. The Azerbaijan Grand Prix (Azerbaijani: Azərbaycan Qran Prisi) is a Formula One motor racing event that was held for the first time in 2017. It is held on the Baku City Circuit, a street circuit in Baku, the capital of Azerbaijan.`,
+      img: '.././images/baku.jpg',
+      maps:
+        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3039.621416397609!2d49.85099291566789!3d40.37291807937018!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40307db472eb07d1%3A0x529ac68af9a1cec2!2sBaku%20City%20Circuit!5e0!3m2!1ssr!2srs!4v1631487357484!5m2!1ssr!2srs'
+    },
+    {
+      id: 4,
+      headerTitle: 'Red Bull Ring',
+      headerSubTitle: 'Red Bull Ring',
+      headerText: 'The Red Bull Ring is a motorsport race track in Spielberg, Styria, Austria.',
+      title: 'Red Bull Ring',
+      description:
+        `The Red Bull Ring is a motorsport race track in Spielberg, Styria, Austria.
+
+        The race circuit was founded as Österreichring (translation: Austrian Circuit) and hosted the Austrian Grand Prix for 18 consecutive years, from 1970 to 1987. It was later shortened, rebuilt and renamed the A1-Ring (A Eins-Ring), and it hosted the Austrian Grand Prix again from 1997 to 2003. When Formula One outgrew the circuit, a plan was drawn up to extend the layout. Parts of the circuit, including the pits and main grandstand, were demolished, but construction work was stopped and the circuit remained unusable for several years before it was purchased by Red Bull's Dietrich Mateschitz and rebuilt. Renamed the Red Bull Ring the track was reopened on 15 May 2011 and subsequently hosted a round of the 2011 DTM season and a round of the 2011 F2 championship. Formula One returned to the circuit in the 2014 season, and MotoGP returned to the circuit in the 2016 season. The Red Bull Ring also hosted a second F1 event named the Styrian Grand Prix in 2020 and 2021; and a second MotoGP event named the Styrian motorcycle Grand Prix in 2020 and 2021 after the COVID-19 Pandemic affected the schedules of both of those seasons.`,
+      img: '.././images/rbr.jpg',
+      maps:
+        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2709.816075900061!2d14.76299771586075!3d47.220180979160915!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4771ceb42b30363d%3A0x7798d905e0e60c3b!2sRed%20Bull%20Ring!5e0!3m2!1ssr!2srs!4v1631487496117!5m2!1ssr!2srs'
+    },
+    {
+      id: 5,
+      headerTitle: 'Spa Circuit',
+      headerSubTitle: 'Circuit de Spa-Francorchamps',
+      headerText: `The Circuit de Spa-Francorchamps, frequently referred to as Spa, is a motor-racing circuit located in Stavelot, Belgium. It is the current venue of the Formula One Belgian Grand Prix, hosting its first Grand Prix in 1925, and has held a Grand Prix every year since 1985 (except 2003 and 2006).`,
+      title: 'Spa Circuit',
+      description:
+        `The Circuit de Spa-Francorchamps, frequently referred to as Spa, is a motor-racing circuit located in Stavelot, Belgium. It is the current venue of the Formula One Belgian Grand Prix, hosting its first Grand Prix in 1925, and has held a Grand Prix every year since 1985 (except 2003 and 2006).
+
+        Spa also hosts several other international events including the 24 Hours of Spa, the World Endurance Championship 6 Hours of Spa-Francorchamps, and the TCR Spa 500. It is also home to the Uniroyal Fun Cup 25 Hours of Spa, one of the longest motor races in the world.
+        
+        The circuit has undergone several redesigns through its history, most extensively in 1979 when the track was modified and shortened from a 14.10 km (8.76 mi) circuit using public roads to a 6.95 km (4.32 mi) permanent circuit due to safety concerns with the old circuit.
+        
+        Despite its name, the circuit is not in Spa but lies in the vicinity of the town of Francorchamps within the boundaries of the municipality of Stavelot, with a part in the boundaries of Malmedy.`,
+      img: '.././images/spa.jpg',
+      maps:
+        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2541.2248811698564!2d5.969861315959356!3d50.43691177947356!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c07b5c0460f737%3A0x78105e8819c35c0e!2sCircuit%20de%20Spa-Francorchamps!5e0!3m2!1ssr!2srs!4v1631487650983!5m2!1ssr!2srs'
+    },
+    {
+      id: 6,
+      headerTitle: 'Monza Circuit',
+      headerSubTitle: 'Autodromo Nazionale di Monza',
+      headerText:
+        `The Autodromo Nazionale di Monza is a historic race track near the city of Monza, north of Milan, in Italy. Built in 1922, it was the world's third purpose-built motor racing circuit after Brooklands and Indianapolis. The circuit's biggest event is the Italian Grand Prix. With the exception of the 1980 running, the race has been hosted there since 1949.`,
+      title: 'Monza Circuit',
+      description:
+        `The Autodromo Nazionale di Monza is a historic race track near the city of Monza, north of Milan, in Italy. Built in 1922, it was the world's third purpose-built motor racing circuit after Brooklands and Indianapolis.[dubious – discuss][5] The circuit's biggest event is the Italian Grand Prix. With the exception of the 1980 running, the race has been hosted there since 1949.[6]
+
+        Built in the Royal Villa of Monza park in a woodland setting, the site has three tracks – the 5.793-kilometre (3.600 mi) Grand Prix track, the 2.405-kilometre (1.494 mi) Junior track, and a 4.250-kilometre (2.641 mi) high speed oval track with steep bankings which was left unused for decades and had been decaying until it was restored in the 2010s. The major features of the main Grand Prix track include the Curva Grande, the Curva di Lesmo, the Variante Ascari and the Curva Parabolica. The high speed curve, Curva Grande, is located after the Variante del Rettifilo which is located at the end of the front straight or Rettifilo Tribune, and is usually taken flat out by Formula One cars.`,
+      img: '.././images/monza.jpg',
+      maps:
+        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2790.4448553182633!2d9.282611315813611!3d45.62179897910317!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4786ba360e48bd7d%3A0x645e7ef5a9d3a632!2sAutodromo%20Nazionale%20Monza!5e0!3m2!1ssr!2srs!4v1631487767448!5m2!1ssr!2srs'
+    }
+  ];
+  
+  export const news = [
+    {
+      id: 1,
+      newsTitle: 'Ricciardo leads stunning McLaren 1-2 after Verstappen and Hamilton collide at Monza',
+      newsText:
+        'A dramatic crash between title protagonists Lewis Hamilton and Max Verstappen that took both drivers out of the race cleared the way for Daniel Ricciardo to claim a sensational victory in the Italian Grand Prix, giving McLaren their first race win since the 2012 Brazilian Grand Prix – and a one-two to boot, as he led home team mate Lando Norris, with Valtteri Bottas taking third, in a thrilling race at Monza.'
+    },
+    {
+      id: 2,
+      newsTitle: 'Formula 1 confirms 2021 Japanese Grand Prix has been cancelled',
+      newsText:
+        'Formula 1 has announced that the 2021 Japanese Grand Prix has been cancelled, due to the ongoing Covid-19 situation in the country.'
+    },
+    {
+      id: 3,
+      newsTitle: 'Australian Grand Prix off in 2021',
+      newsText:
+        'The Australian Grand Prix will not take place in 2021. A spokesperson for Formula 1 said that while they are disappointed that event can’t take place they “are confident we can deliver a 23-race season in 2021 and we have a number of options to take forward to replace the place left vacant by the Australian Grand Prix.'
+    },
+    {
+      id: 4,
+      newsTitle: 'Turkish Grand Prix to rejoin 2021 Formula 1 calendar',
+      newsText:
+        'Formula 1 have announced that the Turkish Grand Prix will return to the calendar this season, with the race at Istanbul Park set to take place in early October, on the weekend originally scheduled to host the Singapore Grand Prix.'
+    },
+    {
+      id: 5,
+      newsTitle: 'Miami Grand Prix to join F1 calendar in 2022, with exciting new circuit planned',
+      newsText:
+        'Formula 1 is heading back to Florida, with the news that the brand-new Miami Grand Prix will join the F1 calendar in 2022. The race – set to be the first since 1959 in the US state – will be held on an exciting new layout at the Hard Rock Stadium complex in Miami Gardens, home to the NFL’s famous Miami Dolphins franchise.'
+    },
+    {
+      id: 6,
+      newsTitle: 'TECH TUESDAY: Why Zandvoort’s banking left Mercedes playing catch-up to Red Bull',
+      newsText:
+        'In the ongoing contest between 2021’s two fastest cars, the Mercedes W12 and Red Bull RB16B, Zandvoort proved to be perfectly configured for the high downforce traits of the latter and presented some difficulties for the Mercedes.'
+        
+    }
+  ];
+
+  export const detailInfo = {
+    id: 1,
+    headerTitle: 'Imola Circuit',
+    headerSubTitle: 'Autodromo Internazionale Enzo e Dino Ferrari',
+    headerText:
+      `The Autodromo Internazionale Enzo e Dino Ferrari, better known as Imola, is a motor racing circuit in the town of Imola, in the Emilia-Romagna region of Italy, 40 kilometres (25 mi) east of Bologna. It is one of the few major international circuits to run in an anti-clockwise direction. The circuit is named after Ferrari's late founder, Enzo Ferrari, and his son, Alfredo "Dino" Ferrari, who died in 1956 at age 24. Before Enzo's death, it was called Autodromo Dino Ferrari. The circuit has an FIA Grade One licence.`,
+    title: 'Imola Circuit',
+    description:
+      `The Autodromo Internazionale Enzo e Dino Ferrari, better known as Imola, is a motor racing circuit in the town of Imola, in the Emilia-Romagna region of Italy, 40 kilometres (25 mi) east of Bologna. It is one of the few major international circuits to run in an anti-clockwise direction. The circuit is named after Ferrari's late founder, Enzo Ferrari, and his son, Alfredo "Dino" Ferrari, who died in 1956 at age 24. Before Enzo's death, it was called Autodromo Dino Ferrari. The circuit has an FIA Grade One licence.[2]
+
+      Imola was the venue for the San Marino Grand Prix between 1981 and 2006. During this period, two Grands Prix were held in Italy every year, with the Italian Grand Prix taking place at Monza, so the Imola race was named after the nearby state. Imola also hosted the 1980 Italian Grand Prix in place of Monza. When Formula One visits Imola, it is seen as the home circuit of Scuderia Ferrari, and masses of supporters come out to support the local team.
+      
+      The venue returned to the Formula One calendar during the 2020 season to help the sport fill calendar gaps caused by cancellations of other races due to the COVID-19 pandemic, with the race at the circuit being named the Emilia Romagna Grand Prix in honour of the region where the circuit is located. This also meant the venue hosted a World Championship race under a third different name having hosted the 1980 Italian Grand Prix and the San Marino Grand Prix from 1981 to 2006`,
+    img: '.././images/imola.jpg',
+    maps:
+      'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2853.339630991969!2d11.712162215520328!3d44.34407377910367!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x132b4a7474d71167%3A0x81177e3551c217a6!2z0JDRg9GC0L7QvNC-0LHQuNC70YHQutCwINGB0YLQsNC30LAg0JXQvdGG0L4g0Lgg0JTQuNC90L4g0KTQtdGA0LDRgNC4!5e0!3m2!1ssr!2srs!4v1631487089071!5m2!1ssr!2srs'
+  };
